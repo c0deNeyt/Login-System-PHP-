04b6701a7c5719c96111d1dd0625e5804d4493b1
c0deNeyt <chanchristianarana@gmail.com>
2023-09-09 19:39:24 +0800

    adjust some spaceing and edited page tittle

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <title> Authentication I | WEB</title>
+    <title> Authentication | WEB</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="./image/favicon.png">
     <link rel="stylesheet" href="./css/normalize.css">
@@ -22,20 +22,20 @@
   </head>
   <body>
     <div class="wrapper">
-    <h5 class="card-title">SIGN IN</h5>
+        <h5 class="card-title">SIGN IN</h5>
 <?= validationMsg();?>
-    <form enctype="multipart/form-data"  autocomplete="off" class="form" action="php/process.php" method="post">
-      <input type="hidden" name="signEin" value="signIn">
-      <label>Contact Number:
-        <input value="<?= isset($_POST['cn']) ? htmlspecialchars($_POST['cn'], ENT_QUOTES) : ''; ?>" type="text" name="cn" placeholder="Contact Number">
-      </label>
-      <label class="password">Password:
-        <input value="<?= isset($_POST['pw']) ? htmlspecialchars($_POST['pw'], ENT_QUOTES) : ''; ?>" type="password" name="pw" placeholder="Password">
-      </label>
-      <a class="resetPass signUp" href="#">Forgot Password</a>
-      <a class="signUp" href="php/signup.php">Sign up</a>
-      <input type="submit" class="registerBtn" value="Login">
-    </form>
+        <form enctype="multipart/form-data"  autocomplete="off" class="form" action="php/process.php" method="post">
+          <input type="hidden" name="signEin" value="signIn">
+          <label>Contact Number:
+            <input value="<?= isset($_POST['cn']) ? htmlspecialchars($_POST['cn'], ENT_QUOTES) : ''; ?>" type="text" name="cn" placeholder="Contact Number">
+          </label>
+          <label class="password">Password:
+            <input value="<?= isset($_POST['pw']) ? htmlspecialchars($_POST['pw'], ENT_QUOTES) : ''; ?>" type="password" name="pw" placeholder="Password">
+          </label>
+          <a class="resetPass signUp" href="#">Forgot Password</a>
+          <a class="signUp" href="php/signup.php">Sign up</a>
+          <input type="submit" class="registerBtn" value="Login">
+        </form>
     </div>
   </body>
 </html>
